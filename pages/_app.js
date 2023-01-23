f654f97eb7f8af06d38cacba47bae2bdc41392a8
Leif Gruendler <mistake91@web.de>
2023-01-23 23:21:08 +0100

    create worldmap.js+worldmapbutton

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,18 @@ import GlobalStyle from "@/styles";
 import Head from "next/head";
 import { Grid } from "@/components/homeStyles";
 import styled from "styled-components";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
+
+  function worldmapButton() {
+    if (location.pathname === "/worldmap") {
+      router.back();
+    } else {
+      location.pathname = "/worldmap";
+    }
+  }
 
   return (
     <>
@@ -16,22 +23,13 @@ export default function App({ Component, pageProps }) {
       </Head>
       <Grid>
         <Component {...pageProps} />
-
-        {window.location.href === "http://localhost:3000/worldmap" ? (
-          ""
-        ) : (
-          <StyledWMButton
-            onClick={() => {
-              window.location.href = "./worldmap";
-            }}
-          />
-        )}
+        <WorldmapButton onClick={worldmapButton} />
       </Grid>
     </>
   );
 }
 
-const StyledWMButton = styled.button`
+const WorldmapButton = styled.button`
   width: 25px;
   height: 25px;
   border-radius: 25%;
