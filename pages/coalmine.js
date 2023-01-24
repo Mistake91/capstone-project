bf716e72d531d8cb1e698498d1477f06a7937e21
Leif Gruendler <mistake91@web.de>
2023-01-24 11:05:14 +0100

    create WM links + coalmine

--- pages/coalmine.js
+++ pages/coalmine.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import { Workstation, Character } from "../components/homeStyles";
 
-export default function HomePage() {
+export default function Worldmap() {
   const [characterPosition, setCharacterPositon] = useState({
     row: 9,
     column: 2,
@@ -14,9 +14,9 @@ export default function HomePage() {
         row={characterPosition.row}
         column={characterPosition.column}
       />
+      <Workstation onClick={() => positionHandler(3, 3)} row={3} column={3} />
+      <Workstation onClick={() => positionHandler(6, 4)} row={6} column={4} />
       <Workstation onClick={() => positionHandler(9, 3)} row={9} column={3} />
-      <Workstation onClick={() => positionHandler(9, 4)} row={9} column={4} />
-      <Workstation onClick={() => positionHandler(7, 3)} row={7} column={3} />
     </>
   );
 
