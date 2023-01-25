1cec23f9dbaea8c927905a4777f41188048ecfa1
Leif Gruendler <mistake91@web.de>
2023-01-25 18:33:15 +0100

    first mining tests

--- pages/coalmine.js
+++ pages/coalmine.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { Workstation, Character } from "../components/homeStyles";
 
-export default function Worldmap() {
+export default function Coalmine() {
   const [characterPosition, setCharacterPositon] = useState({
     row: 9,
     column: 2,
@@ -14,9 +14,27 @@ export default function Worldmap() {
         row={characterPosition.row}
         column={characterPosition.column}
       />
-      <Workstation row={3} column={3} onClick={() => positionHandler(3, 3)} />
-      <Workstation row={6} column={4} onClick={() => positionHandler(6, 4)} />
-      <Workstation row={9} column={3} onClick={() => positionHandler(9, 3)} />
+      <Workstation
+        row={3}
+        column={3}
+        onClick={() => {
+          positionHandler(3, 3);
+        }}
+      />
+      <Workstation
+        row={6}
+        column={4}
+        onClick={() => {
+          positionHandler(6, 4);
+        }}
+      />
+      <Workstation
+        row={9}
+        column={3}
+        onClick={() => {
+          positionHandler(9, 3);
+        }}
+      />
     </>
   );
 
